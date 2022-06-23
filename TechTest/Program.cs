d89ac52e548db1d050275948b0864119c7efd0a8
John Gray <johnny@johnnygray.co.uk>
2022-06-23 08:55:03 +0100

    Initial check in of work.

---
+++ TechTest/Program.cs
@@ -0,0 +1,16 @@
+﻿string test = "now()-6Y";
+
+try
+{
+    TechTest.ParseOperationResult parseOperationResult = TechTest.ParseOperation(test);
+
+    Console.WriteLine($"Operator: {parseOperationResult.Operator}");
+    Console.WriteLine($"Count: {parseOperationResult.Count}");
+    Console.WriteLine($"Unit: {parseOperationResult.Unit}");
+}
+catch (TechTest.InvalidOperationException invalidOperationException)
+{
+    Console.WriteLine("Invalid Operation");
+
+    throw invalidOperationException;
+}
---
+++ TechTest/TechTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+public static class TechTest
+{
+    /// <summary>
+    /// Used to ensure that the operation string is in a supported format.
+    /// </summary>
+    //private static Regex validateFormatRegex = new Regex("(now\\(\\))[+-]\\d+(mon\\.?|[smhdy])", RegexOptions.IgnoreCase);
+
+    /// <summary>
+    /// Used validate format and to extract the operator, unit count and unit type from operation strings.
+    /// </summary>
+    private static Regex parseOperationRegex = new Regex("(now\\(\\))(?<operator>[+-])(?<count>\\d+)(?<unit>(mon\\.?|[smhdy]))", RegexOptions.IgnoreCase);
+
+    public class InvalidOperationException : Exception { }
+
+    public struct ParseOperationResult
+    {
+        public string Operator { get; set; }
+
+        public string Count { get; set; }
+
+        public string Unit { get; set; }
+    }
+
+    public static ParseOperationResult ParseOperation(string operation)
+    {
+        Match operationMatch = parseOperationRegex.Match(operation);
+
+        if (!operationMatch.Success)
+        {
+            throw new InvalidOperationException();
+        }
+
+        ParseOperationResult parseOperationResult = new ParseOperationResult
+        {
+            Operator = operationMatch.Groups["operator"].Value,
+            Count = operationMatch.Groups["count"].Value,
+            Unit = operationMatch.Groups["unit"].Value
+        };
+
+        return parseOperationResult;
+    }
+}
